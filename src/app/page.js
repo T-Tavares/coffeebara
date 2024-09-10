8be7c22ef4ec3144ab71ec3702d19de33b48cea2
T-Tavares <thiago.tavares.freire@gmail.com>
2024-09-11 01:09:17 +1200

    - Initial Setup  - 3D model of coffeebara up and running  - Still need some work on textures  - Orbit Camera set  --- Lock on Zoom and Rotation  - Postprocessing added  --- Outline Effect on obects to be clickable

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,55 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import Image from 'next/image';
+import ss from './page.module.scss';
+import {Canvas} from '@react-three/fiber';
+import {useRef} from 'react';
+import {Coffeebara} from './Models/Coffeebara';
+import {OrbitControls, PerspectiveCamera} from '@react-three/drei';
+import {Selection, Select, EffectComposer, Outline} from '@react-three/postprocessing';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    // const cameraRef = useRef();
+    // const handleCameraChange = () => console.log(cameraRef.current);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    const cameraInitPos = [10, 9, -9];
+    const cameraInitRot = [10, 10, -12];
+    const outlineColour = '#04b4ff';
+    return (
+        <div className={ss.page}>
+            <h1>Coffeebara</h1>
+            <div className={ss.modelContainer}>
+                <Canvas>
+                    <directionalLight intensity={0.5} position={[0, 10, 0]} />
+                    <ambientLight intensity={1} />
+
+                    {/* 
+                        Selection is a wrapper that enables the use of postprocessing effects on the children components
+                        here I am using it to outline the clickable meshes
+                    */}
+                    <Selection>
+                        <EffectComposer multisampling={8} autoClear={false}>
+                            <Outline
+                                blur
+                                visibleEdgeColor={outlineColour}
+                                hiddenEdgeColor={outlineColour}
+                                edgeStrength={3}
+                                edgeThickness={10}
+                            />
+                            <Coffeebara />
+                        </EffectComposer>
+                    </Selection>
+
+                    <PerspectiveCamera makeDefault position={cameraInitPos} rotation={cameraInitRot} />
+                    <OrbitControls
+                        minPolarAngle={Math.PI / 3.5}
+                        maxPolarAngle={Math.PI / 2.8}
+                        minAzimuthAngle={Math.PI / 1.7}
+                        maxAzimuthAngle={Math.PI / 1.2}
+                        minDistance={13}
+                        maxDistance={20}
+                    />
+                </Canvas>
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
