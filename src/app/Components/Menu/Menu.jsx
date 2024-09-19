8cff63756212c9d9349c03318606263ef5db8075
T-Tavares <thiago.tavares.freire@gmail.com>
2024-09-19 23:31:40 +1200

    - Fix for Menu not showing images on mobile

--- src/app/Components/Menu/Menu.jsx
+++ src/app/Components/Menu/Menu.jsx
@@ -5,9 +5,7 @@ import Image from 'next/image';
 const MenuItem = ({item}) => {
     return (
         <div className={ss.menuItem}>
-            <div className={ss.image}>
-                <Image src={item.image} alt={menu[0].item} width={80} height={80} />
-            </div>
+            <Image className={ss.image} src={item.image} alt={menu[0].item} width={100} height={100} />
             <h2 className={ss.title}>{item.item}</h2>
             <p className={ss.description}>{item.description}</p>
             <p className={ss.price}>{`$${item.price}`}</p>
